5e48ba7e2173ad957e79d8fae9dea82c6269cfa2
pzhangleo <3991348+pzhangleo@users.noreply.github.com>
2023-11-24 17:18:10 +0800

    change wopan driver
    change to default upload part size 32m version

--- drivers/wopan/driver.go
+++ drivers/wopan/driver.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/Xhofe/wopan-sdk-go"
+	"github.com/pzhangleo/wopan-sdk-go"
 	"github.com/alist-org/alist/v3/internal/driver"
 	"github.com/alist-org/alist/v3/internal/model"
 	"github.com/alist-org/alist/v3/internal/op"
